5666062c8cb43fc1ca9238cc3a1e09b05bd710f9
Omkrishna-git <omdpatil07@gmail.com>
2026-04-20 09:05:15 +0530

    Final assig_1

--- HPC/assig_1.cpp
+++ HPC/assig_1.cpp
@@ -1,173 +1,230 @@
-#include<iostream>
-#include<omp.h>
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <omp.h>
+#include <chrono>
 
 using namespace std;
 
+class Graph {
+public:
+    int vertices, edges;
+    vector<vector<int>> graph;
+    vector<bool> visited;
 
-class Graph{
-    public:
-        // vector<vector<int>> graph;
-        // vector<bool> visited;
-        // int vertices = 0;
-        // int edges = 0;
+    Graph() {
+        cout << "Enter number of vertices: ";
+        cin >> vertices;
 
-        int vertices = 6;
-        int edges = 5;
-        vector<vector<int>> graph = {{1},{0,2,3},{1,4,5},{1,4},{2,3},{2}};
-        vector<bool> visited;
+        cout << "Enter number of edges: ";
+        cin >> edges;
 
-        // Graph(){
-        //     cout << "Enter number of nodes: ";
-        //     cin >> vertices;
-        //     cout << "Enter number of edges: ";
-        //     cin >> edges;
-        //     graph.assign(vertices,vector<int>());
-        //     for(int i = 0 ; i < edges;i++){
-        //         int a,b;
-        //         cout << "Enter adjacent nodes: ";
-        //         cin >> a >> b;
-        //         addEdge(a,b);
-        //     }
-        // }
-        void addEdge(int a, int b){
-            graph[a].push_back(b);
-            graph[b].push_back(a);
+        graph.resize(vertices);
+        visited.resize(vertices, false);
+
+        cout << "Enter edges (u v):\n";
+        for (int i = 0; i < edges; i++) {
+            int u, v;
+            cin >> u >> v;
+            addEdge(u, v);
         }
+    }
 
-        void printGraph(){
-            for(int i = 0; i < vertices; i++){
-                cout << i << " -> ";
-                for(auto j = graph[i].begin(); j != graph[i].end();j++){
-                    cout << *j << " ";
+    void addEdge(int u, int v) {
+        graph[u].push_back(v);
+        graph[v].push_back(u); // undirected
+    }
+
+    void resetVisited() {
+        fill(visited.begin(), visited.end(), false);
+    }
+
+    void printGraph() {
+        cout << "\nAdjacency List:\n";
+        for (int i = 0; i < vertices; i++) {
+            cout << i << " -> ";
+            for (int j : graph[i]) {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // ---------------- SEQUENTIAL DFS ----------------
+    void dfs(int start) {
+        stack<int> s;
+        s.push(start);
+        visited[start] = true;
+
+        while (!s.empty()) {
+            int current = s.top();
+            s.pop();
+
+            cout << current << " ";
+
+            for (int neighbor : graph[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    s.push(neighbor);
                 }
-                cout << endl;
             }
         }
+    }
 
-        void initialize_visited(){
-            visited.assign(vertices,false);
-        }
+    // ---------------- PARALLEL DFS ----------------
+    void parallel_dfs(int start) {
+        stack<int> s;
+        s.push(start);
+        visited[start] = true;
 
-        void dfs(int i){
-            stack<int> s;
-            s.push(i);
-            visited[i] = true;
+        while (!s.empty()) {
+            int current;
 
-            while(s.empty() != true){
-                int current = s.top();
-                cout << current << " ";
-                s.pop();
-                for(auto j = graph[current].begin(); j != graph[current].end();j++){
-                    if(visited[*j] == false){
-                        s.push(*j);
-                        visited[*j] = true;
+            #pragma omp critical
+            {
+                if (!s.empty()) {
+                    current = s.top();
+                    s.pop();
+                    cout << current << " ";
+                }
+            }
+
+            #pragma omp parallel for
+            for (int i = 0; i < graph[current].size(); i++) {
+                int neighbor = graph[current][i];
+
+                if (!visited[neighbor]) {
+                    #pragma omp critical
+                    {
+                        if (!visited[neighbor]) {
+                            visited[neighbor] = true;
+                            s.push(neighbor);
+                        }
                     }
                 }
-                
             }
         }
+    }
 
-        void parallel_dfs(int i){
-            stack<int> s;
-            s.push(i);
-            visited[i] = true;
+    // ---------------- SEQUENTIAL BFS ----------------
+    void bfs(int start) {
+        queue<int> q;
+        q.push(start);
+        visited[start] = true;
 
-            while(s.empty() != true){
-                int current = s.top();
-                cout << current << " ";
-                #pragma omp critical
-                    s.pop();
-                #pragma omp parallel for
-                    for(auto j = graph[current].begin(); j != graph[current].end();j++){
-                        if(visited[*j] == false){
+        while (!q.empty()) {
+            int current = q.front();
+            q.pop();
+
+            cout << current << " ";
+
+            for (int neighbor : graph[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+    }
+
+    // ---------------- PARALLEL BFS ----------------
+    void parallel_bfs(int start) {
+        vector<int> frontier;
+        frontier.push_back(start);
+        visited[start] = true;
+
+        while (!frontier.empty()) {
+            vector<int> next_frontier;
+
+            #pragma omp parallel
+            {
+                vector<int> local_next;
+
+                #pragma omp for nowait
+                for (int i = 0; i < frontier.size(); i++) {
+                    int current = frontier[i];
+
+                    cout << current << " ";
+
+                    for (int neighbor : graph[current]) {
+                        if (!visited[neighbor]) {
                             #pragma omp critical
                             {
-                                s.push(*j);
-                                visited[*j] = true;
+                                if (!visited[neighbor]) {
+                                    visited[neighbor] = true;
+                                    local_next.push_back(neighbor);
+                                }
                             }
                         }
                     }
-                
-            }
-        }
-
-        void bfs(int i){
-            queue<int> q;
-            q.push(i);
-            visited[i] = true;
-
-            while(q.empty() != true){
-                int current = q.front();
-                q.pop();
-                cout << current << " ";
-                for(auto j = graph[current].begin(); j != graph[current].end();j++){
-                    if(visited[*j] == false){
-                        q.push(*j);
-                        visited[*j] = true;
-                    }
                 }
-            }
-        }
 
-        void parallel_bfs(int i){
-            queue<int> q;
-            q.push(i);
-            visited[i] = true;
-
-            while(q.empty() != true){
-                
-                    int current = q.front();
-                    cout << current << " ";
-                    #pragma omp critical
-                        q.pop();
-                    
-                #pragma omp parallel for
-                    for(auto j = graph[current].begin(); j != graph[current].end();j++){
-                        if(visited[*j] == false){
-                            #pragma omp critical
-                                q.push(*j);
-                                visited[*j] = true;
-                        }
-                    }
-                
+                #pragma omp critical
+                next_frontier.insert(next_frontier.end(),
+                                     local_next.begin(),
+                                     local_next.end());
             }
+
+            frontier = next_frontier;
         }
+    }
 };
 
-int main(int argc, char const *argv[])
-{
+// ---------------- MAIN MENU ----------------
+int main() {
     Graph g;
-    cout << "Adjacency List:\n";
     g.printGraph();
-    g.initialize_visited();
-    cout << "Depth First Search: \n";
-    auto start = chrono::high_resolution_clock::now();
-    g.dfs(0);
-    cout << endl;
-    auto end = chrono::high_resolution_clock::now();
-    cout << "Time taken: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << " microseconds" << endl;
-    cout << "Parallel Depth First Search: \n";
-    g.initialize_visited();
-    start = chrono::high_resolution_clock::now();
-    g.parallel_dfs(0);
-    cout << endl;
-    end = chrono::high_resolution_clock::now();
-    cout << "Time taken: "<< chrono::duration_cast<chrono::microseconds>(end - start).count() << " microseconds" << endl;
-    start = chrono::high_resolution_clock::now();
-    cout << "Breadth First Search: \n";
-    g.initialize_visited();
-    g.bfs(0);
-    cout << endl;
-    end = chrono::high_resolution_clock::now();
-    cout << "Time taken: "<< chrono::duration_cast<chrono::microseconds>(end - start).count() << " microseconds" << endl;
-    start = chrono::high_resolution_clock::now();
-    cout << "Parallel Breadth First Search: \n";
-    g.initialize_visited();
-    g.parallel_bfs(0);
-    cout << endl;
-    end = chrono::high_resolution_clock::now();
-    cout << "Time taken: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << " microseconds" << endl;
+
+    int choice, start;
+
+    do {
+        cout << "\n===== MENU =====\n";
+        cout << "1. DFS\n";
+        cout << "2. Parallel DFS\n";
+        cout << "3. BFS\n";
+        cout << "4. Parallel BFS\n";
+        cout << "5. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        if (choice >= 1 && choice <= 4) {
+            cout << "Enter starting node: ";
+            cin >> start;
+
+            g.resetVisited();
+
+            auto start_time = chrono::high_resolution_clock::now();
+
+            switch (choice) {
+                case 1:
+                    cout << "DFS: ";
+                    g.dfs(start);
+                    break;
+
+                case 2:
+                    cout << "Parallel DFS: ";
+                    g.parallel_dfs(start);
+                    break;
+
+                case 3:
+                    cout << "BFS: ";
+                    g.bfs(start);
+                    break;
+
+                case 4:
+                    cout << "Parallel BFS: ";
+                    g.parallel_bfs(start);
+                    break;
+            }
+
+            auto end_time = chrono::high_resolution_clock::now();
+
+            cout << "\nTime taken: "
+                 << chrono::duration_cast<chrono::microseconds>(
+                        end_time - start_time)
+                        .count()
+                 << " microseconds\n";
+        }
+
+    } while (choice != 5);
 
     return 0;
 }
